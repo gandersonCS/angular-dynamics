13740bf1cfb0b1604c8a0876d6da9abc8dc31710
gandersonCS <ganderson@ksu.edu>
2026-09-16 23:36:15 -0500

    done

--- ShipSprite.cs
+++ ShipSprite.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Content;
+
 
 namespace AngularDynamicsExercise;
 
@@ -10,10 +12,18 @@ namespace AngularDynamicsExercise;
 /// </summary>
 public class ShipSprite
 {
+    const float LINEAR_ACCELERATION = 10;
+    const float ANGULAR_ACCELERATION = 5;
+
+
     public Game game;
     public Texture2D texture;
     public Vector2 position;
     public Vector2 velocity;
+    Vector2 direction;
+
+    float angle;
+    float angularVelocity;
 
     /// <summary>
     /// Creates the ship sprite
@@ -22,6 +32,7 @@ public class ShipSprite
     {
         this.game = game;
         this.position = new Vector2(375, 250);
+        this.direction = -Vector2.UnitY;
     }
 
     /// <summary>
@@ -42,12 +53,37 @@ public class ShipSprite
         KeyboardState keyboardState = Keyboard.GetState();
         float t = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
+        Vector2 acceleration = new Vector2(0, 0);
+        float angularAcceleration = 0;
+        if (keyboardState.IsKeyDown(Keys.A))
+        {
+            acceleration += direction * LINEAR_ACCELERATION;
+   
+            angularAcceleration += ANGULAR_ACCELERATION;
+        }
+        if (keyboardState.IsKeyDown(Keys.D))
+        {
+            acceleration += direction * LINEAR_ACCELERATION;
+        
+            angularAcceleration -= ANGULAR_ACCELERATION;
+        }
+        
+        angularVelocity += angularAcceleration * t;
+        angle += angularVelocity * t;
+        direction.X = (float)Math.Sin(angle);
+        direction.Y = (float)Math.Cos(angle);
+
+        velocity += acceleration * t;
+        position += velocity * t;
         // Wrap the ship to keep it on-screen
         var viewport = game.GraphicsDevice.Viewport;
         if (position.Y < 0) position.Y = viewport.Height;
         if (position.Y > viewport.Height) position.Y = 0;
         if (position.X < 0) position.X = viewport.Width;
         if (position.X > viewport.Width) position.X = 0;
+
+        velocity += acceleration * t;
+        position += velocity * t;
     }
 
     /// <summary>
@@ -57,7 +93,7 @@ public class ShipSprite
     /// <param name="spriteBatch">The SpriteBatch to draw with</param>
     public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(texture, position, Color.White);
+        spriteBatch.Draw(texture, position, null, Color.White, angle, new Vector2(30, 39), 1f, SpriteEffects.None, 0f);
     }
 }
 
